eeaec5c409610152cc9de5262bfe27bac79ce9f9
Bushra Nikhat <bushranik6@gmail.com>
2021-08-24 17:04:15 +0530

    some modification to pdata

--- src/components/portfolio/Pdata.js
+++ src/components/portfolio/Pdata.js
@@ -1,14 +1,14 @@
 const Pdata=[
     {
         id:"1",
-        image:"/assests/digilocker.jpg",
+        image:"assests/digilocker.jpg",
         name:"Internship",
         linkName:"https://drive.google.com/file/d/1Ut26avr8FRKiupgmTWrrTcVMbuVeDhzZ/view?usp=sharing"
 }
   ,
     {
         id:"2",
-        image:"/assests/pizza.jpg",
+        image:"assests/pizza.jpg",
         name:"Pizza App",
         linkName:"https://github.com/BushraNikhat/pizzaHub"
 }
